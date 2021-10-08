06e9c48040caa5f14b243cc0e95f40a3f7c51959
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-08 19:29:14 +0200

    Impl Challenges Controller

---
+++ src/Web/Controllers/ChallengesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YbHackathon.Solutioneers.Web.Models;
+using YbHackathon.Solutioneers.Web.Services.Interfaces;
+
+namespace YbHackathon.Solutioneers.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ChallengesController : ControllerBase
+    {
+        private readonly IChallengeService _challengeService;
+
+        public ChallengesController(IChallengeService challengeService)
+        {
+            _challengeService = challengeService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Challenge> Get()
+        {
+            return _challengeService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Challenge> GetById([FromQuery] Guid id)
+        {
+            return _challengeService.GetById(id);
+        }
+    }
+}
