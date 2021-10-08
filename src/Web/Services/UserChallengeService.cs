ce42bfe5d5fc841ba6ff6207e93032ae448fd582
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-08 22:07:03 +0200

    minor

--- src/Web/Services/UserChallengeService.cs
+++ src/Web/Services/UserChallengeService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using YbHackathon.Solutioneers.Web.Data;
