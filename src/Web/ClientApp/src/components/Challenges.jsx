19d45d7a0a246b6fe5f0f8118d9bd09c5a61e795
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2021-10-08 19:27:56 +0200

    Move fetch to home.jsx

--- src/Web/ClientApp/src/components/Challenges.jsx
+++ src/Web/ClientApp/src/components/Challenges.jsx
@@ -1,100 +1,23 @@
-import authService from "./api-authorization/AuthorizeService";
-import React, { useState, useEffect } from "react";
-import { Spinner } from "reactstrap";
+import React from "react";
 import { ChallengeCard } from "./ChallengeCard";
 import { ChallengeCatalog } from "./ChallengeCatalog";
 
-export const Challenges = ({ onSelected }) => {
-  const [activeChallenges, setActiveChallenges] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // declare the async data fetching function
-    const fetchData = async () => {
-      const token = await authService.getAccessToken();
-      const response = await fetch("weatherforecast", {
-        headers: !token ? {} : { Authorization: `Bearer ${token}` },
-      });
-      let data = await response.json();
-
-      data = [
-        {
-          id: "abcd",
-          topic: "Travel",
-          title: "Travel Challenge",
-          Description: "",
-          PointsToEarn: 2,
-          OpenFrom: "",
-          OpenTo: "",
-        },
-        {
-          id: "abcd3",
-          topic: "Food",
-          title: "Food Challenge",
-          Description: "",
-          PointsToEarn: 2,
-          OpenFrom: "",
-          OpenTo: "",
-        },
-        {
-          id: "abcd2",
-          topic: "Home",
-          title: "Home Challenge",
-          Description: "",
-          PointsToEarn: 2,
-          OpenFrom: "",
-          OpenTo: "",
-        },
-        {
-          id: "abcd1",
-          topic: "Stuff",
-          title: "Challenge",
-          Description: "",
-          PointsToEarn: 2,
-          OpenFrom: "",
-          OpenTo: "",
-        },
-      ];
-
-      // set state with the result
-      setActiveChallenges(data);
-      setIsLoading(false);
-    };
-
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-  }, []);
-
-  const renderActiveChallenges = (challenges) => {
-    return (
-      <>
-        {challenges.map((challenge) => (
-          <ChallengeCard
-            key={challenge.id}
-            challenge={challenge}
-            onSelected={() => onSelected(challenge, true)}
-          />
-        ))}
-      </>
-    );
-  };
-
-  return (
-    <div>
-      <h2>Challenges</h2>
-      {isLoading ? (
-        <Spinner type="grow" color="info" />
-      ) : (
-        <>
-          {renderActiveChallenges(activeChallenges)}
-          <div className="h-divider-50"></div>
-          <ChallengeCatalog
-            onShowChallengeDetails={(challenge) => onSelected(challenge, false)}
-          />
-        </>
-      )}
-    </div>
-  );
-};
+export const Challenges = ({
+  userData,
+  onActiveChallengeSelected,
+  onNewChallengeSelected,
+}) => (
+  <>
+    {userData.challenges.map((challenge) => (
+      <ChallengeCard
+        key={challenge.id}
+        challenge={challenge}
+        onSelected={() => onActiveChallengeSelected(challenge)}
+      />
+    ))}
+    <div className="h-divider-50"></div>
+    <ChallengeCatalog
+      onShowChallengeDetails={(challenge) => onNewChallengeSelected(challenge)}
+    />
+  </>
+);
--- src/Web/ClientApp/src/components/Home.jsx
+++ src/Web/ClientApp/src/components/Home.jsx
@@ -1,19 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ChallengeDetails } from "./ChallengeDetails";
 import { Challenges } from "./Challenges";
 import { Scores } from "./Scores";
+import authService from "./api-authorization/AuthorizeService";
+import { Spinner } from "reactstrap";
 
 export const Home = () => {
   const [selectedChallenge, setSelectedChallenge] = useState(undefined);
-  const [isSelectedChallengeActive, setIsSelectedChallengeActive] =
-    useState(undefined);
+  const [userData, setUserData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (selectedChallenge) {
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      const token = await authService.getAccessToken();
+      const response = await fetch("weatherforecast", {
+        headers: !token ? {} : { Authorization: `Bearer ${token}` },
+      });
+      let data = await response.json();
+
+      data = {
+        challenges: [
+          {
+            id: "abcd",
+            topic: "Travel",
+            title: "Travel Challenge",
+            Description: "",
+            PointsToEarn: 2,
+            OpenFrom: "",
+            OpenTo: "",
+          },
+          {
+            id: "abcd3",
+            topic: "Food",
+            title: "Food Challenge",
+            Description: "",
+            PointsToEarn: 2,
+            OpenFrom: "",
+            OpenTo: "",
+          },
+          {
+            id: "abcd2",
+            topic: "Home",
+            title: "Home Challenge",
+            Description: "",
+            PointsToEarn: 2,
+            OpenFrom: "",
+            OpenTo: "",
+          },
+          {
+            id: "abcd1",
+            topic: "Stuff",
+            title: "Challenge",
+            Description: "",
+            PointsToEarn: 2,
+            OpenFrom: "",
+            OpenTo: "",
+          },
+        ],
+      };
+
+      // set state with the result
+      setUserData(data);
+      setIsLoading(false);
+    };
+
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, []);
+
+  if (isLoading) {
+    return <Spinner type="grow" color="info" />;
+  } else if (selectedChallenge) {
     return (
       <ChallengeDetails
-        challenge={selectedChallenge}
+        challenge={selectedChallenge.challenge}
+        isActive={selectedChallenge.isActive}
         onClose={() => setSelectedChallenge(undefined)}
-        isActive={isSelectedChallengeActive}
       />
     );
   } else {
@@ -22,10 +87,13 @@ export const Home = () => {
         <Scores />
         <div className="h-divider-50"></div>
         <Challenges
-          onSelected={(challenge, isActive) => {
-            setSelectedChallenge(challenge);
-            setIsSelectedChallengeActive(isActive);
-          }}
+          userData={userData}
+          onActiveChallengeSelected={(challenge) =>
+            setSelectedChallenge({ challenge, isActive: true })
+          }
+          onNewChallengeSelected={(challenge) =>
+            setSelectedChallenge({ challenge, isActive: false })
+          }
         />
       </>
     );
