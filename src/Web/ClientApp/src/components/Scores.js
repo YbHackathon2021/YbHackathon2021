ea5397ae6ebd8710eb833f131efe7785517f8b4a
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2021-10-08 16:23:50 +0200

    Add empty components

---
+++ src/Web/ClientApp/src/components/Scores.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Card, CardBody, CardTitle } from "reactstrap";
+
+export class Scores extends Component {
+  render() {
+    return (
+      <>
+        <h1>Scores</h1>
+      </>
+    );
+  }
+}
