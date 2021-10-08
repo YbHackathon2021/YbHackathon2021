661a08809c819323049fc548aabfddb44f2a951f
Tom Aebi <thomas.aebi@isolutions.com>
2021-10-08 21:15:55 +0200

    Extend seeding

--- src/Web/Data/ApplicationDbContext.cs
+++ src/Web/Data/ApplicationDbContext.cs
@@ -29,34 +29,79 @@ namespace YbHackathon.Solutioneers.Web.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            Guid Guid001 = new Guid("1AC5D7B1-7DAA-41D8-AAF9-AF0E1E48AE21");
-            Guid Guid002 = new Guid("13D38292-8358-4681-864E-C0915B961BD7");
+            Guid GuidImageCoats = new Guid("1AC5D7B1-7DAA-41D8-AAF9-AF0E1E48AE21");
+            Guid GuidImageLavendel = new Guid("66E754E5-F1C5-49C4-BA80-58791913295E");
+            Guid GuidImageRecycling = new Guid("6B437DCE-8B39-4C84-B2A8-1D9D401E517F");
+            Guid GuidImageSolar = new Guid("6E93C6B8-FB4A-4E51-97D1-D842B8ECA69D");
+            //Guid Guid005 = new Guid("D00A60F5-41DA-4E46-A222-86F373959AB8");
+            Guid GuidChallenge01 = new Guid("930FE3AA-EE36-427C-A819-00EAD0E4ACCF");
+            Guid GuidChallenge02 = new Guid("615DA515-3F0D-4CC0-A9DF-E7A9C880C023");
 
             base.OnModelCreating(builder);
             System.Drawing.ImageConverter _imageConverter = new System.Drawing.ImageConverter();
 
-            System.Drawing.Image imageCoatd = System.Drawing.Image.FromFile("Resources/Coats.png");
+            System.Drawing.Image imageCoats = System.Drawing.Image.FromFile("Resources/Coats.png");
+            System.Drawing.Image imageLavendel = System.Drawing.Image.FromFile("Resources/Lavendel.png");
+            System.Drawing.Image imageRecycling = System.Drawing.Image.FromFile("Resources/Recycling.png");
+            System.Drawing.Image imageSolar = System.Drawing.Image.FromFile("Resources/Solar.png");
 
             builder.Entity<Image>().HasData(
                 new Image
                 {
-                    Id = Guid001,
-                    Data = (byte[])_imageConverter.ConvertTo(imageCoatd, typeof(byte[]))
+                    Id = GuidImageCoats,
+                    Data = (byte[])_imageConverter.ConvertTo(imageCoats, typeof(byte[]))
                 }
             );
 
+            builder.Entity<Image>().HasData(
+                new Image
+                {
+                    Id = GuidImageLavendel,
+                    Data = (byte[])_imageConverter.ConvertTo(imageLavendel, typeof(byte[]))
+                }
+            );
+
+            builder.Entity<Image>().HasData(
+                new Image
+                {
+                    Id = GuidImageRecycling,
+                    Data = (byte[])_imageConverter.ConvertTo(imageRecycling, typeof(byte[]))
+                }
+            );
+
+            builder.Entity<Image>().HasData(
+                new Image
+                {
+                    Id = GuidImageSolar,
+                    Data = (byte[])_imageConverter.ConvertTo(imageSolar, typeof(byte[]))
+                }
+            );
 
             builder.Entity<Challenge>().HasData(
                 new Challenge
                 {
-                    Id = Guid002,
-                    Title = "1st Challende",
-                    Description = "Text, text, text, text, text, text, text",
-                    Topic = Topic.Food,
+                    Id = GuidChallenge01,
+                    Title = "Already heard about secondhand stores?",
+                    Description = "Buying Seconhand clothes saves resources. Human and ecological resources. I claim that secondhand clothing is the most sustainable way to buy clothes, because no new resources are consumed. Offer for sale clothes that are already in circulation. It is important not to fall into a consumption frenzy because it is secondhand anyway. Only what really pleases and is needed, but above all also gives pleasure, should be bought.",
+                    Topic = Topic.Stuff,
                     PointsToEarn = 3,
                     OpenFrom = new DateTime(2021, 10, 1),
                     OpenTo = new DateTime(2021, 10, 20),
-                    ImageId = Guid001
+                    ImageId = GuidImageCoats
+                }
+            );
+
+            builder.Entity<Challenge>().HasData(
+                new Challenge
+                {
+                    Id = GuidChallenge02,
+                    Title = "Producing your own energy is cool",
+                    Description = "Solar systems, so-called photovoltaic systems, for the roof bring with them numerous advantages and disadvantages. On the one hand, users get free electricity and contribute to environmental protection; on the other hand, they have to accept expensive acquisition costs and a fluctuating yield. In terms of wear and tear, the advantages and disadvantages balance each other out. This article presents in detail the various arguments in favor and against the purchase of solar panels for the roof.",
+                    Topic = Topic.Home,
+                    PointsToEarn = 3,
+                    OpenFrom = new DateTime(2021, 10, 1),
+                    OpenTo = new DateTime(2021, 10, 20),
+                    ImageId = GuidImageSolar
                 }
             );
         }
