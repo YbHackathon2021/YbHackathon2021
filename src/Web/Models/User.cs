bf1c1af7ee1d2cb0c98ae2d82c8e2aa73b8ef89f
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-08 22:23:43 +0200

    init user obj

--- src/Web/Models/User.cs
+++ src/Web/Models/User.cs
@@ -5,8 +5,8 @@ namespace YbHackathon.Solutioneers.Web.Models
     public class User : BaseEntity
     {
         public string ApplicationUserId { get; set; }
-        public List<Achievement> Achievements { get; set; }
-        public List<Score> Scores { get; set; }
-        public List<UserChallenge> UserChallenges { get; set; }
+        public List<Achievement> Achievements { get; set; } = new List<Achievement>();
+        public List<Score> Scores { get; set; } = new List<Score>();
+        public List<UserChallenge> UserChallenges { get; set; } = new List<UserChallenge>()
     }
 }
