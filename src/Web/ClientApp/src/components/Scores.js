57466459792b42c9b3f9f81a9cbc3a194d61e456
Tom Aebi <thomas.aebi@isolutions.com>
2021-10-08 16:59:51 +0200

    Scores 1st idea

--- src/Web/ClientApp/src/components/Scores.js
+++ src/Web/ClientApp/src/components/Scores.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import { Card, CardBody, CardTitle } from "reactstrap";
+import { Card, CardBody, CardTitle, Progress  } from "reactstrap";
 
 
 const Scores = () => {
     return (
         <>
             <h1>Scores</h1>
+
+            <div className="text-left">Food:</div>
+            <Progress animated value={10} />
+
+            <div className="text-left">Home:</div>
+            <Progress animated color="success" value={25} />
+
+            <div className="text-left">Travel:</div>
+            <Progress animated color="warning" value={50} />
+
+            <div className="text-left">Stuff:</div>
+            <Progress animated color="danger" value={75} />
         </>
     );
 }
