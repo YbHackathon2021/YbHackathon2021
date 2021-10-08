195ab78829408317586d1b1150fb3b1e1e2fa089
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-08 22:23:55 +0200

    minor

--- src/Web/Models/User.cs
+++ src/Web/Models/User.cs
@@ -7,6 +7,6 @@ namespace YbHackathon.Solutioneers.Web.Models
         public string ApplicationUserId { get; set; }
         public List<Achievement> Achievements { get; set; } = new List<Achievement>();
         public List<Score> Scores { get; set; } = new List<Score>();
-        public List<UserChallenge> UserChallenges { get; set; } = new List<UserChallenge>()
+        public List<UserChallenge> UserChallenges { get; set; } = new List<UserChallenge>();
     }
 }
