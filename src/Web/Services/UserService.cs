5e87c61d8934bc03bd9084c0ffb2c0c7f52c95a3
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-08 19:24:16 +0200

    Implement getById in user service

--- src/Web/Services/UserService.cs
+++ src/Web/Services/UserService.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using YbHackathon.Solutioneers.Web.Data;
 using YbHackathon.Solutioneers.Web.Models;
 using YbHackathon.Solutioneers.Web.Services.Interfaces;
@@ -10,6 +16,14 @@ namespace YbHackathon.Solutioneers.Web.Services
         public UserService(ApplicationDbContext dbContext, ILogger<UserService> logger) : 
             base(dbContext, logger)
         {
+        }
+
+        public new ActionResult<User> GetById(Guid id)
+        {
+            return dbContext.InternalUsers.Where(u => u.Id == id)
+                .Include(u => u.Achievements)
+                .Include(u => u.Scores)
+                .First();
         }
     }
 }
