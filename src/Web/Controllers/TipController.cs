2321606f5d10b0cb62eb5c0653eaf48826d89de8
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-09 02:14:25 +0200

    Impl tips controller

---
+++ src/Web/Controllers/TipController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YbHackathon.Solutioneers.Web.Models;
+using YbHackathon.Solutioneers.Web.Services.Interfaces;
+
+namespace YbHackathon.Solutioneers.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class TipController : ControllerBase
+    {
+        private readonly ITipService _tipService;
+
+        public TipController(ITipService tipService)
+        {
+            _tipService = tipService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Challenge> Get()
+        {
+            return _tipService.GetAll();
+        }
+    }
+}
