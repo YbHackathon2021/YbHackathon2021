98659d5bdbddaffa50548308027ee5cf9616500c
Marc Rufer <marc.rufer@isolutions.ch>
2021-10-09 02:59:39 +0200

    Get from keyvault

---
+++ src/Web/Controllers/KeysController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace YbHackathon.Solutioneers.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class KeysController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public KeysController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var key = _configuration.GetValue<string>("Tenor:ApiKey");
+            return new List<string>
+            {
+                key
+            };
+        }
+    }
+}
